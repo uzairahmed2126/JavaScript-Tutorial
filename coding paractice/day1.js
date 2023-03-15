813f4ae8969555906cc86e263375d5239de7653a
uzairahmed <uzairahmed21261@gamail.com>
2023-03-16 02:42:43 +0530

    practice work

--- coding paractice/day1.js
+++ coding paractice/day1.js
@@ -36,14 +36,65 @@ function convert(conv){
 
 
 // 4 ==>
-function giveMeSomething(str){
-return String("something ")+str;
+// function giveMeSomething(str){
+// return String("something ")+str;
+// }
+// console.log(giveMeSomething("is better than nothing"));
+// console.log(giveMeSomething("Bob Jane"));
+// console.log(giveMeSomething("something"));
+
+// x = "is better than nothing";
+// giveMeSomething(x);
+
+
+function addition(num1,num2){
+    return Number(num1)+Number(num2);
+    // return (+num1) + (+num2)
+    // return parseInt(num1)+parseInt(num2);
+    // return (Number(num1)+(Number(num2)))
+
+
 }
-console.log(giveMeSomething("is better than nothing"));
-console.log(giveMeSomething("Bob Jane"));
-console.log(giveMeSomething("something"));
+// console.log(addition(3, 2))
+// console.log(addition(-3, -6))
+// console.log(addition(7, 3))
+// console.log(addition('7', '3'))
+// console.log(addition('7', 3))
 
-x = "is better than nothing";
-giveMeSomething(x);
+function convert(multiply){
+  return multiply*60
+}
+// console.log(convert(5))
+// console.log(convert(3))
+// console.log(convert(2))
+// console.log(convert('2'))
+
+// 3 ==>
+function addition(value){
+    // return (+value)+1;
+    // return Number(value)+1
+    // return parseInt(value)+1
+}
+// console.log(addition(0))
+// console.log(addition('9'))
+// console.log(addition('-3'))
 
 
+// 4 ==>
+function giveMeSomething(str){
+    // return String("Something ")+str;
+    // return ("Something ")+str
+    // return (`something ${str} `)
+
+}
+//  console.log(giveMeSomething("is better than nothing"));
+//  console.log(giveMeSomething("Bob Jane")); 
+//  console.log(giveMeSomething("something")); 
+
+// Area of a Triangle
+function triArea(base,height){
+   return 0.5*(base*height);
+}
+console.log(triArea(3, 2))
+console.log(triArea(7, 4))
+console.log(triArea(10, 10))
