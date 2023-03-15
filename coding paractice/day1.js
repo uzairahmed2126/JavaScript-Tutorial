518bd4692a1b014e40818150bad70a61ba6cca95
uzairahmed <uzairahmed21261@gamail.com>
2023-03-16 01:11:23 +0530

    practice work

---
+++ coding paractice/day1.js
@@ -0,0 +1,49 @@
+function addition(num1,num2){
+    // return Number(num1)+Number(num2);
+    // return num1 * num2;
+    // return (+num1)+(+num2);
+    // return parseInt(num1) +parseInt(num2);
+    // return parseFloat(num1) + (+num2)
+
+}
+
+// console.log(addition(3, 2));
+// console.log(addition(-3, -6));
+// console.log(addition(7, 3) );
+// console.log(addition('7', '3'));
+// console.log(addition('7', 3));
+
+// 2 ==>
+
+function convert(conv){
+    // return Number(conv*60)
+    // return conv*60;
+     
+}
+//   console.log(convert(5));
+//   console.log(convert(3));
+//   console.log(convert(2));
+//   console.log(convert('2'));
+
+// 3 ==>
+// function addition(add){
+//     // return Number(add)+1;
+//     return parseInt(add)+1;
+// }
+//   console.log(addition(0));
+//   console.log(addition('9'));
+//   console.log(addition('-3'));
+
+
+// 4 ==>
+function giveMeSomething(str){
+return String("something ")+str;
+}
+console.log(giveMeSomething("is better than nothing"));
+console.log(giveMeSomething("Bob Jane"));
+console.log(giveMeSomething("something"));
+
+x = "is better than nothing";
+giveMeSomething(x);
+
+
