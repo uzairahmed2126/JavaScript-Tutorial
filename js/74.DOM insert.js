902634609f67fe084a5ad47f8d7a6383bbe4748e
uzairahmed <uzairahmed21261@gamail.com>
2023-03-16 23:27:24 +0530

    Adjacent methods

--- js/74.DOM insert.js
+++ js/74.DOM insert.js
@@ -2,10 +2,10 @@ let newElement = document.createElement("h3");
 let newText    = document.createTextNode("oyee sunn sunnn");
 newElement.appendChild(newText)
 let target     = document.getElementById("insert-id");
-// target.insertAdjacentElement("afterbegin",newElement );
-// target.insertAdjacentElement("beforebegin",newElement );
-// target.insertAdjacentElement("afterend",newElement );
-// target.insertAdjacentElement("beforeend",newElement );
+target.insertAdjacentElement("afterbegin",newElement );
+target.insertAdjacentElement("beforebegin",newElement );
+target.insertAdjacentElement("afterend",newElement );
+target.insertAdjacentElement("beforeend",newElement );
 
 let triggered = document.querySelector(".insert-DOM")
 let newtrig = "<h3>oyee sun</h3>"
