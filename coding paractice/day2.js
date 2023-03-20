4b4bb5c4a9d209091c12197646073b21e1553c80
uzairahmed <uzairahmed21261@gamail.com>
2023-03-21 02:40:57 +0530

    count animal legs

--- coding paractice/day2.js
+++ coding paractice/day2.js
@@ -21,7 +21,25 @@ function footballPoints(wins,draws,losses){
 function isSameNum(num1,num2){
     return Object.is(num1,num2);
 }
-console.log(isSameNum(4,8))
-console.log(isSameNum(2,2))
-console.log(isSameNum(2,"2"))
-console.log(isSameNum(0,-0))
+// console.log(isSameNum(4,8));
+// console.log(isSameNum(2,2));
+// console.log(isSameNum(2,"2"));
+// console.log(isSameNum(0,-0));
+
+// 4.==> The Farm Problem
+//     In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
+//         chickens = 2 legs
+//         cows = 4 legs
+//         pigs = 4 legs
+//     The farmer has counted his animals and he gives you a subtotal for each species. You have to implement a function that returns the total number of legs of all the animals.
+//     animals(2, 3, 5) ➞ 36
+//     animals(1, 2, 3) ➞ 22
+//     animals(5, 2, 8) ➞ 50
+//     Remember that the farmer wants to know the total number of legs and not the total number of animals.
+
+function animals(chickens,cows,pigs){
+    return 2*chickens+(4)*cows+(4)*pigs
+}
+console.log(animals(2,3,5))
+console.log(animals(1,2,3))
+console.log(animals(5,2,8))
