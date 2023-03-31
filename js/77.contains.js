c1af8d48019050a2f76bda0b54d229b32cd0f862
uzairahmed <uzairahmed21261@gamail.com>
2023-04-01 01:15:10 +0530

    contains method

--- js/77.contains.js
+++ js/77.contains.js
@@ -3,3 +3,9 @@ let parantel = document.querySelector("ul");
 let target = document.getElementById("unknown");
 let find = parantel.contains(target);
 console.log(find);
+
+
+let check = document.querySelector("input");
+let haveornot = document.getElementById("inptid");
+let yey = check.contains(haveornot)
+console.log(yey)
