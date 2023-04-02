98b2e1f033b1a327ba8a33c37d3324a5d4951de5
uzairahmed <uzairahmed21261@gamail.com>
2023-04-02 21:42:38 +0530

    isEqualNode

--- js/79.isEqualNode.js
+++ js/79.isEqualNode.js
@@ -2,4 +2,7 @@ let eql  = document.querySelector("div").firstElementChild;
 let eql2 = document.querySelector("ul").firstElementChild;
 let comp = eql.isEqualNode(eql2);
 console.log(comp);
-
+let nt = document.querySelector("div").firstElementChild;
+let nt2 = document.querySelector("#fruits");
+let check = nt.isEqualNode(nt2);
+console.log(check)
