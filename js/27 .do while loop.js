a7cb4530e8d551c1a5aadff94713e9de9e6a851d
uzairahmed <uzairahmed21261@gamail.com>
2023-05-22 20:29:57 +0530

    do while

--- js/27 .do while loop.js
+++ js/27 .do while loop.js
@@ -6,7 +6,13 @@ do{
 
 let b=10;
 do{
-    alert("This is not real it's just a dream please wake up wake up wake up every soul will taste death");
+    // alert("This is not real it's just a dream please wake up wake up wake up every soul will taste death");
 b--;
 }while(b>=1)
 
+let DO = 10;
+
+do {
+    console.log("hello boi");
+    DO--;
+}while(DO>=1);
