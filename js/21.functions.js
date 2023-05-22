246f5613811d4c08d4d7e471a5428bd2b31e84e7
uzairahmed <uzairahmed21261@gamail.com>
2023-05-22 18:49:11 +0530

    function

--- js/21.functions.js
+++ js/21.functions.js
@@ -16,5 +16,26 @@
 function fn(r,r1) {
     return r + r1 , r-r1 ,r1 / r , r1*r;
 }
-// console.log(fn());
-console.log(fn(4 , 4));
+
+function Try() {
+    // document.querySelector('.try').addEventListener('click');
+    // return alert("good after Noon");
+}
+
+function fn_call() {
+    console.log(fn(5,45));
+    Try();
+}
+fn_call();
+
+// This statement is aligible for exicute but at the end that's throw error
+function first() {
+    console.log("hey i am first");
+     sec();
+}
+function sec() {
+    console.log("hey i am second");
+     first();
+}
+first();
+// sec();
