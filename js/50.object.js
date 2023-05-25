08f28e377b67f9c57db423a992cb24cf1893ee31
uzairahmed <uzairahmed21261@gamail.com>
2023-05-25 19:19:28 +0530

    Constructor

--- js/50.object.js
+++ js/50.object.js
@@ -23,9 +23,32 @@
 function person(name,age) {
     this.name = name;
     this.age =   age;
+    this.yearsUntilRetire = yearLeft;
 }
+// let the1 = new person("my name is AI",2023);
+// let the2 = new person("my name is ",20);
+// document.write(the1.name);
+// document.write(the1.age);
+function yearLeft () {
+    let numYears = 65 - this.age;
+    if(numYears===0 ) {
+        alert("you gone retired");
+    }
+    return numYears;
+}
+let here = new person("JavaScript",28);
+let there = new person("Python",56);
+let y = new person("C#",65);
+let z = new person("Java",20);
+let x = new person("C++",20);
+console.log(here.yearsUntilRetire());
+console.log(here.name);
+console.log(there.yearsUntilRetire());
+console.log(there.name);
+console.log(y.yearsUntilRetire());
+console.log(y.name);
+console.log(z.yearsUntilRetire());
+console.log(z.name);
+console.log(x.yearsUntilRetire());
+console.log(x.name);
 
-let the1 = new person("my name is AI",2023);
-let the2 = new person("my name is ",20);
-document.write(the1.name);
-document.write(the1.age);
