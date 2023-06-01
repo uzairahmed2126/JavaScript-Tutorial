5683734b560c562f16c7436f239532131513a1b6
uzairahmed <uzairahmed21261@gamail.com>
2023-06-01 22:24:18 +0530

    Accessing Form Elements

--- js/61.document object module.js
+++ js/61.document object module.js
@@ -1,2 +1,20 @@
-let x = document.forms[0].length;
-console.log(x);
+// let x = document.forms[0].length;
+// document.write(" "+x+" ");
+
+// let y = document.forms[0].elements[2].value;
+// document.write(y);
+
+// let z = document.forms[0].elements[1].name;
+// document.write(" "+z+" ");
+
+function html_type() {
+  let Type = document.getElementById('btn')
+  Type = prompt('How Color you want on click')
+  if (Type === (document.style.color = 'black')) {
+    document.getElementById('form-container').style.background = 'black'
+    document.getElementById('h2').style.background = 'white'
+    // document.write(' ' + Type + ' ')
+  } else {
+    alert('write only black and white')
+  }
+}
