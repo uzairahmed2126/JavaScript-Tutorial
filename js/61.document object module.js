31121ef7e8e06f918a89c0996412786ec8697f76
uzairahmed <uzairahmed21261@gamail.com>
2023-06-01 23:15:40 +0530

    Accessing Form Elements

--- js/61.document object module.js
+++ js/61.document object module.js
@@ -1,23 +1,23 @@
-// let x = document.forms[0].length;
-// document.write(" "+x+" ");
+let x = document.forms[0].length;
+document.write(" "+x+" ");
 
-// let y = document.forms[0].elements[2].value;
-// document.write(y);
+let y = document.forms[0].elements[2].value;
+document.write(y);
 
-// let z = document.forms[0].elements[1].name;
-// document.write(" "+z+" ");
+let z = document.forms[0].elements[1].name;
+document.write(" "+z+" ");
 
-// function html_type() {
-//   let Type = document.getElementById('btn')
-//   Type = prompt('How Color you want on click')
-//   if (Type === (document.style.color = 'black')) {
-//     document.getElementById('form-container').style.background = 'black'
-//     document.getElementById('h2').style.background = 'white'
-//     // document.write(' ' + Type + ' ')
-//   } else {
-//     alert('write only black and white')
-//   }
-// }
+function html_type() {
+  let Type = document.getElementById('btn')
+  Type = prompt('How Color you want on click')
+  if (Type === (document.style.color = 'black')) {
+    document.getElementById('form-container').style.background = 'black'
+    document.getElementById('h2').style.background = 'white'
+    // document.write(' ' + Type + ' ')
+  } else {
+    alert('write only black and white')
+  }
+}
 
 function validator() {
     if(document.forms[0].elements[0].checked) {
