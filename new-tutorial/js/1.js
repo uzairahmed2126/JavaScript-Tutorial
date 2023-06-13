b6adca0bad2faf8744ba57284f5201e9abf40b18
uzairahmed2126 <--global>
2023-06-13 17:20:02 +0530

    .

---
+++ new-tutorial/js/1.js
@@ -0,0 +1,14 @@
+// When I write script source at head part then it is naccecary to write attribute defer,
+//  The Defer it is read document and then exicute
+const get_id = document.getElementById('h');
+get_id.innerHTML='Uzair Ahmed';
+// let a = prompt('What is your name');
+// console.log(a);
+// let hello = a;
+// alert('Your Name is '+hello);
+
+// let Bgcolor = document.getElementById('try').style.background=prompt('Enter Background color name')
+// let color = document.getElementById('try').style.color=prompt('Enter color name')
+// let fontSize = document.getElementById('try').style.fontSize=prompt('Enter font size')
+// let fontfamily = document.getElementById('try').style.background=prompt('Enter color name')
+// console.log(color,fontSize); 
