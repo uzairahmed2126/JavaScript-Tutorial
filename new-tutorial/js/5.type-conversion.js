517ed95fb2b9c520b4cef3230ea2a2d344d866be
uzairahmed2126 <--global>
2023-06-16 22:41:55 +0530

    concat

--- new-tutorial/js/5.type-conversion.js
+++ new-tutorial/js/5.type-conversion.js
@@ -8,10 +8,25 @@
 // let NewType = Number(type)
 // console.log(typeof NewType)
 
-let boolean = true;
+let boolean = true
 console.log(typeof boolean)
 let NewBoolean = Number(boolean)
 console.log(NewBoolean)
 console.log(typeof NewBoolean)
 console.log(NewBoolean)
-console.log(typeof String(boolean));
+console.log(typeof String(boolean))
+
+// let str = prompt('Enter user name')
+// if (str === '$' && str === '%') {
+//   console.log('equal')
+// } else {
+//   console.log('not equal')
+// }
+
+// concat
+let a = 'first'
+let b = 'second'
+console.log(a+" "+b);
+
+let ConCat =a.concat(' ', b);
+console.log(ConCat);
