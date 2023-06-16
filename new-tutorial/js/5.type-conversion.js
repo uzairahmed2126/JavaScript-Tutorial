01d3b7fc1ab93a96db9a4b91e16789ddc2c883f7
uzairahmed2126 <--global>
2023-06-16 23:06:16 +0530

    substring

--- new-tutorial/js/5.type-conversion.js
+++ new-tutorial/js/5.type-conversion.js
@@ -30,3 +30,10 @@ console.log(a+" "+b);
 
 let ConCat =a.concat(' ', b);
 console.log(ConCat);
+
+let str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+let substr = str.substring(0,5);
+// console.log("length of string is: ", str.length);
+// let substr = str.split('dolor');
+// let substr = str[0];
+console.log(substr)
