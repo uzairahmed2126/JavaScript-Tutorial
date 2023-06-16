af406484aca97b38a18265a1009e62f7896627d7
uzairahmed2126 <--global>
2023-06-16 23:27:45 +0530

    indexof,lastindex of

--- new-tutorial/js/6.string-methods.js
+++ new-tutorial/js/6.string-methods.js
@@ -6,13 +6,16 @@ console.log(a + ' ' + b)
 let ConCat = a.concat(' ', b)
 console.log(ConCat)
 
-let str = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+let str = 'This is JavaScript tutorial. It is a good tutorial'
 let substr = str.substring(0, 5)
 // console.log("length of string is: ", str.length);
 // let substr = str.split('dolor');
 // let substr = str[0];
-console.log(substr)
+console.log(substr.length)
 
 // find index
 let index = str.indexOf('good');
 console.log(index) 
+
+let LASTindex =str.lastIndexOf('l')
+console.log(LASTindex)
