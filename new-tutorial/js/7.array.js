81c7181b0194d13d2db9fd6e4bb8fffab46a01c1
uzairahmed2126 <--global>
2023-06-20 18:36:10 +0530

    function

--- new-tutorial/js/7.array.js
+++ new-tutorial/js/7.array.js
@@ -14,7 +14,6 @@ for (let i = 0; i < books.length; i++) {
   console.log(books[i])
 }
 
-
 console.log(typeof console) //object
 console.log(typeof null) //object
 console.log(typeof undefined) //undefined
@@ -52,41 +51,4 @@ function myfu(a) {
 let b = books.forEach((element) => {
   console.log(element)
 })
-
-// function
-let wholeBody = (document.ondrag = function () {
-  Muliply()
-})
-function Muliply() {
-  // for(i=1;i<=10;i++) {
-  //     document.write(`2 x ${i} = ${2*i}<br>`)
-  // }
-  alert('Assalamu Alykum')
-}
-let make = document.createElement('a')
-make.textContent = 'drag me'
-document.body.appendChild(make)
-
-// let creat = document.createElement('table')
-// function calculation(addnum){
-//     for(let i=1;i<=10;i++) {
-//       // document.write(`${addnum} x ${i} = ${addnum*i}<br>`)
-//      prompt('Enter tr number and name').document.createElement('tr');
-//      prompt('Enter td number and name') . document.createElement('td');
-      
-//   }
-// }
-// calculation(prompt('enter any number'));
-
-function add() {
-  let sum = 0;
-  for (var i = 0; i < arguments.length; i++){
-    sum= sum+arguments[i];
-
-  }
-  console.log(sum)
-}
-
-// add(prompt("enter arguments"));
-add(4,6);
 
