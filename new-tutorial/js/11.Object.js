782f25982161225fee3bb6e8c5d65750168573b2
uzairahmed2126 <--global>
2023-06-23 18:47:21 +0530

    for in

--- new-tutorial/js/11.Object.js
+++ new-tutorial/js/11.Object.js
@@ -1,13 +1,37 @@
 // object should have properties
 let person = {
-    'Firstname' : 'Uzair',
-    Secname   :  'Ahmed',
-    height : 's'
-};
-console.log(person['Firstname'])
-console.log(person.Secname)
-
-person.age= '19'
+  Firstname: 'Uzair',
+  Secname: 'Ahmed',
+  license : 'no',
+  sayHello() {
+    console.log('helo')
+}
+}
+// console.log(person['Firstname'])
+// console.log(person.Secname)
+person.sayHello();
+person.age = '19'
 delete person.age
-person.Firstname='ali'
-console.log('height'in person)
+person.Firstname = 'ali'
+
+// The in method do return true and false
+console.log('Firstname' in person)
+
+for (const key in person) {
+//   if (Object.hasOwnProperty.call(person, key)) {
+    // const element = person[key]
+    // console.log(element)
+//   }
+// console.log(key,person[key])
+}
+
+// method in object
+person.sayHello = function(){
+    console.log('hello!');
+}
+
+function fncname(){
+    console.log('oye');
+}
+
+// person.sayHello = fncname;
